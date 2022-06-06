1318199e6c6258363a1ec73d7cae5ec6d172a352
Isaac Ahuvia <isaacahuvia@gmail.com>
2022-06-06 09:22:47 -0400

    Fix "raw" dataset, enhance symptom beliefs analyses

--- PPD Data Prep.R
+++ PPD Data Prep.R
@@ -10,7 +10,8 @@ rm(list = ls())
 
 
 ####  Load Data  ####
-raw <- read.csv("S:\\TRACK to TREAT\\Data\\Processed Data\\cleaned_qualtrics_ttt_phase_1.csv", stringsAsFactors = F)
+# raw <- read.csv("S:\\TRACK to TREAT\\Data\\Processed Data\\cleaned_qualtrics_ttt_phase_1.csv", stringsAsFactors = F)
+raw <- read.csv("S:\\TRACK to TREAT\\Data\\Processed Data\\cleaned_qualtrics_ttt_phase_1_fixed_220606.csv", stringsAsFactors = F)
 
 
 
